604fc24361a478c947d23c3ae81f3571089e6eef
Benito Palacios Sanchez <pleonex@protonmail.com>
2024-10-05 13:33:38 +0200

    ✨Implement client factory

---
+++ src/PleOps.Splitwise.Client/SplitwiseClientFactory.cs
@@ -0,0 +1,50 @@
+﻿namespace PleOps.Splitwise.Client;
+
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+/// <summary>
+/// Factory to create clients of the Splitewise API.
+/// </summary>
+public static class SplitwiseClientFactory
+{
+    /// <summary>
+    /// Create a new client with the api key authentication.
+    /// </summary>
+    /// <param name="apiKey">Personal API key for authentication.</param>
+    /// <param name="options">Options for the client.</param>
+    /// <returns>New client for the Splitewise API.</returns>
+    public static SplitwiseClient CreateWithApiKeyAuth(string apiKey, SplitwiseClientOptions? options = null)
+    {
+        options ??= new SplitwiseClientOptions();
+        IRequestOption[] httpOptions = [ options.RetryOptions ];
+
+        // Set the base address in the http client if required.
+        // In this case the default value comes from the openapi spec.
+        HttpClient httpClient = KiotaClientFactory.Create(null, httpOptions);
+
+        var apiKeyAuth = new ApiKeyAuthenticationProvider(
+            "Authorization",
+            $"Bearer {apiKey}",
+            ApiKeyAuthenticationProvider.KeyLocation.Header);
+
+        var adapter = new HttpClientRequestAdapter(new BaseBearerTokenAuthenticationProvider(new Test(apiKey)), httpClient: httpClient);
+
+        return new SplitwiseClient(adapter);
+    }
+
+    private sealed class Test(string apiKey) : IAccessTokenProvider
+    {
+        public AllowedHostsValidator AllowedHostsValidator => throw new NotImplementedException();
+
+        public Task<string> GetAuthorizationTokenAsync(Uri uri, Dictionary<string, object>? additionalAuthenticationContext = null, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(apiKey);
+        }
+    }
+}
---
+++ src/PleOps.Splitwise.Client/SplitwiseClientOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace PleOps.Splitwise.Client;
+
+using System.Net;
+using Microsoft.Kiota.Http.HttpClientLibrary.Middleware.Options;
+
+/// <summary>
+/// Options for the Splitwise client.
+/// </summary>
+public class SplitwiseClientOptions
+{
+    /// <summary>
+    /// Gets or sets the options for the retry handler of the client.
+    /// </summary>
+    public RetryHandlerOption RetryOptions { get; set; } = new() {
+        ShouldRetry = (_, _, r) =>
+            r.StatusCode is >= HttpStatusCode.InternalServerError or HttpStatusCode.RequestTimeout,
+    };
+}
